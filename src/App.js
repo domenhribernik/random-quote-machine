1b9b7292ac290e9b38ad2afa8d2deb46303bd160
domenhribernik <domen.hribernik4@gmail.com>
2020-10-26 20:50:21 +0100

    Adding project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+  constructor() {
+    super();
+    this.state = {
+      test: "Loading...",
+      author: "Loading..."
+    }
+    this.newQuote = this.newQuote.bind(this);
+  }
+
+  componentDidMount() {
+    this.newQuote();
+  }
+
+  newQuote = () => {
+    fetch("https://type.fit/api/quotes")
+        .then(response => response.json())
+        .then(data => {
+            let r = Math.floor(Math.random() * data.length)
+            console.log(data[r])
+            this.setState({
+                text: data[r].text,
+                author: data[r].author
+            })
+        })
+  }
+
+  render() {
+    return (
+      <div id="quote-box">
+        <div id="text">{this.state.text}</div>
+        <div id="author">{this.state.author == null ? "- Unknown" : "- " + this.state.author}</div>
+        <div class="buttonContainer">
+          <a href="https://twitter.com/intent/tweet" id="tweet-quote" target="_black"><i class="fa fa-twitter"></i></a>
+          <button id="new-quote" onClick={this.newQuote}>New Quote</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
